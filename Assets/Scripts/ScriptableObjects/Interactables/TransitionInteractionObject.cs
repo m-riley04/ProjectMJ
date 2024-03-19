96d0667aef251fb602c253966ef3774689f0fcf6
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-19 16:08:41 -0500

    Create TransitionInteractionObject.cs

---
+++ Assets/Scripts/ScriptableObjects/Interactables/TransitionInteractionObject.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Transition Interaction Object", menuName = "Interactables/Transition Interaction")]
+public class TransitionInteractionObject : ScriptableObject
+{
+    public string transitionScene;
+}
