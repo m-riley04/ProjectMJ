721f908b91cb9886544209a919dc335c1b601419
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-23 23:54:15 -0500

    added OnCameraCapture for Camera item

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.IO;
 
 public class Item : MonoBehaviour
 {
     public ItemObject item;
     public Light itemLight;
+    public Camera itemCamera;
+    public int FileCounter = 0;
 
     public void Start()
     {
@@ -24,6 +27,37 @@ public class Item : MonoBehaviour
     {
         if (itemLight) {
             itemLight.enabled = !itemLight.enabled;
+        } else { print("Light not found on item."); }
+    }
+    public void OnCameraCapture()
+    {
+        if (itemCamera)
+        {
+            RenderTexture currentRT = RenderTexture.active;
+            RenderTexture.active = itemCamera.targetTexture;
+
+            itemCamera.Render();
+
+            Texture2D Image = new Texture2D(itemCamera.targetTexture.width, itemCamera.targetTexture.height);
+            Image.ReadPixels(new Rect(0, 0, itemCamera.targetTexture.width, itemCamera.targetTexture.height), 0, 0);
+            Image.Apply();
+            RenderTexture.active = currentRT;
+
+            var Bytes = Image.EncodeToPNG();
+            Destroy(Image);
+
+            // Check if directory doens't exist
+            if (!Directory.Exists(Application.dataPath + "/CameraPictures"))
+            {
+                // Create it
+                Directory.CreateDirectory(Application.dataPath + "/CameraPictures");
+            }
+
+            File.WriteAllBytes(Application.dataPath + "/CameraPictures/" + FileCounter + ".png", Bytes);
+            FileCounter++;
+        } else
+        {
+            print("Camera not found on item");
         }
     }
 
