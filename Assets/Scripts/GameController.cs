55eb83f29e151f5ddf424b4320dc80bd49beb52d
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-23 23:55:58 -0500

    added getters and setters for some game settings
    a

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -6,15 +6,47 @@ using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public Mission currentMission;
+    [Header("Player")]
+    public Player player;
+
+    [Header("Missions")]
+    public MissionObject currentMission;
+
+    [Header("Settings")]
+    [Header("Video")]
+    [Range(0, 100)] public float postProcesingAmount = 100;
+    [Range(30, 120)] public float fov           = 90;
+
+    [Header("Sound")]
+    [Range(0, 100)] public float volume         = 100;
+    [Range(0, 100)] public float volumeSFX      = 100;
+    [Range(0, 100)] public float volumeMusic    = 100;
+    [Range(0, 100)] public float volumeBeings   = 100;
 
     void Awake()
     {
         DontDestroyOnLoad(gameObject);
     }
 
-    public void StartNewGame()
+    public void setVolume(float val)
     {
-        SceneManager.LoadScene("Lobby");
+        volume = val;
+    }
+    public void setVolumeSFX(float val)
+    {
+        volumeSFX = val;
+    }
+    public void setVolumeMusic(float val)
+    {
+        volumeMusic = val;
+    }
+    public void setVolumeBeings(float val)
+    {
+        volumeBeings = val;
+    }
+
+    public void setPostProcess(float val)
+    {
+        postProcesingAmount = val;
     }
 }
