30509d6fc31897d5db1a99974faa8ae5ea2be9f8
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-23 23:50:55 -0500

    add check for GameController

--- Assets/Scripts/PlayerSpawnpoint.cs
+++ Assets/Scripts/PlayerSpawnpoint.cs
@@ -5,12 +5,23 @@ using UnityEngine;
 public class PlayerSpawnpoint : MonoBehaviour
 {
     public GameObject playerPrefab;
+    public GameObject gameControllerPrefab;
 
     void Start()
     {
+        // Check if gamecontroller already exists
+        if (!FindObjectOfType<GameController>())
+        {
+            // If it doesn't, create it
+            GameObject gameController = Instantiate(gameControllerPrefab, transform, false);
+            gameController.transform.parent = null;
+            DontDestroyOnLoad(gameController);
+        }
+
         // Check if player already exists
         if (!FindObjectOfType<Player>())
         {
+            // If it doesn't, create it
             GameObject player = Instantiate(playerPrefab, transform, false);
             player.transform.parent = null;
             DontDestroyOnLoad(player);
