5ecdbdd3515b103ccb1243e83a93a503257978e9
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-26 15:28:42 -0500

    move enumerators to data classes

--- Assets/Scripts/ScriptableObjects/Missions/MissionObject.cs
+++ Assets/Scripts/ScriptableObjects/Missions/MissionObject.cs
@@ -3,45 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum CrashCause
-{
-    None,
-    Weather,
-    Weaponry,
-    ElectricalFailure,
-    PurposfulLanding,
-    Other
-}
-
-public enum Weather
-{
-    None,
-    Rain,
-    Thunderstorm,
-    Foggy,
-    MeteorShower,
-    Other
-}
-
-public enum Difficulty
-{
-    Easy,
-    Medium,
-    Hard,
-    TopSecret,
-    Other
-}
-
-public enum CraftType
-{
-    None,
-    Saucer,
-    TicTac,
-    Cigar,
-    Sphere,
-    Other
-}
-
 [CreateAssetMenu(fileName = "New Mission", menuName = "Missions/Mission")]
 public class MissionObject : ScriptableObject
 {
