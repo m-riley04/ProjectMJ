b70ac1599f0d5eab0380896301e93c084f4ab9e3
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-03-26 15:27:50 -0500

    Create SaveLoadSystem.cs

---
+++ Assets/Scripts/SaveLoadSystem.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using Newtonsoft.Json;
+
+public class SaveLoadSystem
+{
+    public static string ScriptableObjectToJSON(ScriptableObject obj)
+    {
+        return Newtonsoft.Json.JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        });
+    }
+
+    public static void Save<T>(T obj, string path)
+    {
+
+        // Write json text
+        File.WriteAllText(path, Newtonsoft.Json.JsonConvert.SerializeObject(obj, new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        }));
+    }
+
+    public static T Load<T>(string path)
+    {
+        string json = File.ReadAllText(path);
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<T>(json);
+    }
+
+}
